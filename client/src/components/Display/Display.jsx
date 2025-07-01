9e4eb7d9c8ccddaef999263b00c7143bb3373bd4
Shank34 <spikbang@gmail.com>
2025-07-01 16:41:57 +0530

    first commit

---
+++ client/src/components/Display/Display.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../Navandside/Navbar'
+
+const Display = () => {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default Display
---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Display from '../components/Display/Display'
+
+const MainPage = () => {
+  return (
+    <>
+      <Display/>
+    </>
+  )
+}
+
+export default MainPage
