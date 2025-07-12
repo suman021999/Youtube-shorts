e15c54b08e5430c4aa9d28d050c98e8686e8f1a2
Shank34 <spikbang@gmail.com>
2025-07-12 15:13:50 +0530

    description

---
+++ client/src/components/auth/g_login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <div>
+       <div className="w-10 h-10 bg-red-800 text-amber-50 flex items-center justify-center rounded-full">
+         BE
+       </div>
+      </div>
+    </>
+  )
+}
+
+export default Login
