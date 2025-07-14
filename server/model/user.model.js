ec88528534516f864f1692d9a47d9f4417eecc41
Shank34 <spikbang@gmail.com>
2025-07-14 20:17:05 +0530

    searchbar,middleware,model

---
+++ server/model/user.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+    },
+  },
+
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
+
+
+
