8e79781de958b6e5db8c1daa45d4d82bd2705e01
Shank34 <spikbang@gmail.com>
2025-08-23 13:04:35 +0530

    02:google oauth

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,17 @@ import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./hooks/store.js";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+<Provider store={store}>
       <Router>
         <App />
       </Router>
     </Provider>
+    </GoogleOAuthProvider>
+    
   </StrictMode>
 );
